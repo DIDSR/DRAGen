9ffcafb9dfdea22631f2cefbc8ca8b48055608cc
Burgon <alburgon2@gmail.com>
2023-06-08 16:16:56 -0400

    updated args.py such that different arguments are required depending on current use case.

--- src/args.py
+++ src/args.py
@@ -1,25 +1,26 @@
 from argparse import ArgumentParser
 
-def get_arguments():
+def get_arguments(mode):
   parser = ArgumentParser()
   # Define Arguments ==========================================================================================
-  ## Required inputs ------------------------------------------------------------------------------------------
-  parser.add_argument('--model_file', type=str, required=True)
-  parser.add_argument('--data_csv', type=str, required=True)
+  # # Arguments that are always used
   parser.add_argument('--save_loc', type=str, required=True)
-  ## General settings -----------------------------------------------------------------------------------------
   parser.add_argument('--save_name', type=str, default='decision_regions')
-  parser.add_argument('--batch_size', type=int, default=5)
-  parser.add_argument('--out_function', type=str, default=None)
-  parser.add_argument('--overwrite', action='store_true', default=False) # NOTE: will delete all data in hdf5 file at beginning of running
-  ## Vicinal distribution settings ----------------------------------------------------------------------------
-  parser.add_argument('--steps', type=int, default=5, 
+  
+  if mode == 'Generate' or mode == 'Complete':
+    ## Arguments used to generate and evaluate vicinal distributions ------------------------------------------
+    parser.add_argument('--model_file', type=str, required=True)
+    parser.add_argument('--data_csv', type=str, required=True)
+    parser.add_argument('--batch_size', type=int, default=5)
+    parser.add_argument('--overwrite', action='store_true', default=False) # NOTE: will delete all data in hdf5 file at beginning of running
+    parser.add_argument('--steps', type=int, default=5, 
     help="Number of 'steps' to take between images in generating the vicinal distribution")
-  ## Triplet manager settings ---------------------------------------------------------------------------------
-  parser.add_argument('--n_triplets', type=int, default=5)
-  parser.add_argument('--img_rel_path', type=str, default=None)
-  ## Analysis settings ----------------------------------------------------------------------------------------
-  parser.add_argument('--aggregate', '--agg', default=None, choices=['group', 'class', 'all'])
+    parser.add_argument('--n_triplets', type=int, default=5)
+    parser.add_argument('--img_rel_path', type=str, default=None)
+  if mode == 'Analyze' or mode == 'Complete':
+    ## Arguments used to perform decision region analysis -----------------------------------------------------
+    parser.add_argument('--out_function', type=str, default=None)
+    parser.add_argument('--aggregate', '--agg', default=None, choices=['group', 'class', 'all'])
   # Process Arguments =========================================================================================
   args = parser.parse_args()
   if not args.save_name.endswith("hdf5"):
