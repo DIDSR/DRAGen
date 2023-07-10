f9099e30e9b6166802f523b7bef3bc216bf862dc
Burgon <alburgon2@gmail.com>
2023-07-10 12:09:47 -0400

    Added plotting arguments

--- src/args.py
+++ src/args.py
@@ -1,4 +1,5 @@
 from argparse import ArgumentParser
+from src.utils import verify_type_or_none
 
 #classes = {"COVID_positive":['Yes','No']}
 #subgroup_attributes = {"sex":['F','M']}
@@ -44,11 +45,21 @@ class CustomParser():
       self.parser.add_argument('--out_function', type=str, default=None)
       self.parser.add_argument('--aggregate', '--agg', default=None, choices=['group', 'class', 'all'])
       ### Plotting arguments
-      self.parser.add_argument('--plot', default=None, choices=['composition', 'performance'])
+      self.parser.add_argument('--plot', default=None, choices=['composition', 'performance', 'region'])
       self.parser.add_argument('--show', default=False, action='store_true')
-      self.parser.add_argument('--display-only', dest='display_only', default=False, action='store_true') # Don't save plots (+ other outputs?)
+      self.parser.add_argument('--display-only', dest='save_plot', default=True, action='store_false') # Don't save plots (+ other outputs?)
       self.parser.add_argument('--hide-percent', dest='show_percent', default=True, action='store_false')
       self.parser.add_argument('--hide-errorbar', dest='show_errorbar', default=True, action='store_false')
+      self.parser.add_argument("--save_dpi", "--save-dpi", default=800, type=int, help="dpi used when saving summary plots.")
+      self.parser.add_argument("--plot_output_format","--plot-output-format", default=[], nargs='+',
+        help="Output formats in which the plots should be saved.")
+      self.parser.add_argument("--plot_palette",'--plot-palette', type=str, default="Set2",
+        help="Color palette to be used during plotting, can be either a matplotlib colorpalette or a custom palette.")
+      #### Only used with plot == 'region
+      self.parser.add_argument("--plot_threshold", "--plot-threshold", default=None,
+        help="Threshold applied to ouput scores in 'region' plots; if None, no threshold is applied.")
+      self.parser.add_argument("--n_per_group", '--n-per-group', default=None,
+        help="Number of decision regions to plot per group if plot = 'region'; if None, all decision regions are plotted.")
   
   def parse_args(self):
     args = self.parser.parse_args()
@@ -60,5 +71,14 @@ class CustomParser():
     args.tasks = {}
     for c in args.classes:
       args.tasks[c] = {float(args.class_order[i]):args.classes[c][i] for i in range(len(args.classes[c]))}
+    args.plot_threshold = verify_type_or_none(args.plot_threshold, float, arg_name="--plot-threshold/--plot_threshold")
+    args.n_per_group = verify_type_or_none(args.n_per_group, int, "--n-per-group/--n_per_group")
+    args.plot_output_format = list(set(args.plot_output_format))
+    if len(args.plot_output_format) == 0:
+      args.plot_output_format = ['png']
+    if ":" in args.plot_palette:
+      args.plot_palette = {c.split(":")[0]:c.split(":")[-1] for c in args.plot_palette.split(",")}
+    elif ',' in args.plot_palette:
+      args.plot_palette = args.plot_palette.split(",")
     return args
   
